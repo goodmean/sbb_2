3e6d31b978808f2b37b309d3d8b58d4cc6de986b
goodmean <zizigoo12@gmail.com>
2022-05-24 23:23:07 +0900

    2-04 엔티티, mariadb 연동하기

--- src/main/java/com/mysite/sbb/Answer.java
+++ src/main/java/com/mysite/sbb/Answer.java
@@ -11,19 +11,16 @@ import java.util.List;
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
-
-	@Column(length = 200)
-	private String subject;
 
 	@Column(columnDefinition = "TEXT")
 	private String content;
 
 	private LocalDateTime createDate;
 
-	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-	private List<Answer> answerList;
+	@ManyToOne
+	private Question question;
 }
