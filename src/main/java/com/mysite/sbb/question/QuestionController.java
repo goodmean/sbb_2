baaa40e4a594c0fd8059507d50934bbd29ffc2a1
goodmean <zizigoo12@gmail.com>
2022-06-01 22:33:55 +0900

    2-09 서비스

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -12,11 +11,11 @@ import java.util.List;
 @Controller
 public class QuestionController {
 
-	private final QuestionRepository questionRepository;
+	private final QuestionService questionService;
 
 	@RequestMapping("/question/list")
 	public String list(Model model) {
-		List<Question> questionList = this.questionRepository.findAll();
+		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
 	}
