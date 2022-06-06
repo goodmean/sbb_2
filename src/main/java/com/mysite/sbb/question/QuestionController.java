3d7c3080fc40d9c5c624f1cdddcfc0d32d247b90
goodmean <zizigoo12@gmail.com>
2022-06-06 23:47:02 +0900

    2-15 질문 등록과 폼, 폼

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -1,10 +1,16 @@
 package com.mysite.sbb.question;
 
+import com.mysite.sbb.answer.AnswerForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.validation.Valid;
 import java.util.List;
 
 @RequestMapping("/question")
@@ -22,20 +28,23 @@ public class QuestionController {
 	}
 
 	@RequestMapping(value = "/detail/{id}")
-	public String detail(Model model, @PathVariable("id") Integer id) {
+	public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
 	}
 
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam String subject, @RequestParam String content) {
-		this.questionService.create(subject, content);
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list"; // 질문 저장후 질문목록으로 이동
 	}
 
