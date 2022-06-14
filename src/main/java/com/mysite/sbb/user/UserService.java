3fde1fa2bf623f6cf54814a9dc61941d483938da
goodmean <zizigoo12@gmail.com>
2022-06-14 21:58:28 +0900

    3-08 엔티티 변경

--- src/main/java/com/mysite/sbb/user/UserService.java
+++ src/main/java/com/mysite/sbb/user/UserService.java
@@ -1,12 +1,16 @@
 package com.mysite.sbb.user;
 
+import com.mysite.sbb.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @RequiredArgsConstructor
 @Service
 public class UserService {
+
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
 
@@ -19,5 +23,14 @@ public class UserService {
 		userRepository.save(user);
 
 		return user;
+	}
+
+	public SiteUser getUser(String username) {
+		Optional<SiteUser> siteUser = this.userRepository.findByUsername(username);
+		if (siteUser.isPresent()) {
+			return siteUser.get();
+		} else {
+			throw new DataNotFoundException("siteUser not found");
+		}
 	}
 }
